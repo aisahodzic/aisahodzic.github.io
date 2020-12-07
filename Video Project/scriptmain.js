eb8b921ae0b04ce0007e80b24c662949d3d285bb
Hessa-ala <ha1466@nyu.edu>
2020-12-07 12:06:12 +0400

    updating mainPage elements

---
+++ Video Project/scriptmain.js
@@ -0,0 +1,11 @@
+function check() {
+
+    if (document.getElementById('1').checked){
+    document.getElementById("video-1").checked = "checked";}
+    if (document.getElementById('2').checked){
+    document.getElementById("video-2").checked = "checked";}
+    if (document.getElementById('3').checked){
+    document.getElementById("video-3").checked = "checked";}
+    if (document.getElementById('4').checked){
+    document.getElementById("video-4").checked = "checked";}    
+}
